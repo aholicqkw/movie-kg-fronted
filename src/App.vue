466f594fb3e75281f76228066acaf0d4a7a4aefa
kaiweiqin <kaiwei.qin19@student.xjtlu.edu.cn>
2022-07-27 16:23:03 +0800

    2022.7.27

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<router-view></router-view>
+<LoadingMask></LoadingMask>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoadingMask from './components/loadingMask.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    LoadingMask
+  },
 }
 </script>
 
@@ -19,8 +19,17 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: url(./assets/img/topic_bg.png) no-repeat center center;
+  background-size: cover;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.contentArea {
+  width: 300px;
+  height: 500px;
 }
 </style>
